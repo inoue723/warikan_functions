f6d0f94b413b0eb497dfdee56aa3988e794ddebc
Tatsuro Inoue <omuni723@gmail.com>
2020-06-23 23:37:28 +0900

    add eslint prettier

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,30 @@
-import * as functions from 'firebase-functions';
-import * as line from '@line/bot-sdk';
-import * as express from 'express';
+import * as functions from "firebase-functions";
+import * as line from "@line/bot-sdk";
+import * as express from "express";
 
 const config = {
-    channelAccessToken: functions.config().linebot.token,
-    channelSecret: functions.config().linebot.secret,
+  channelAccessToken: functions.config().linebot.token,
+  channelSecret: functions.config().linebot.secret,
 };
 
 const app = express();
 
 app.post("/costs", line.middleware(config), (req, res) => {
-    return Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result));
-})
+  return Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
 
 const client = new line.Client(config);
 function handleEvent(event: line.WebhookEvent) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        return Promise.resolve(null);
-    }
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
 
-    return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: event.message.text
-    });
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: event.message.text,
+  });
 }
 
 export const lineBot = functions.https.onRequest(app);
